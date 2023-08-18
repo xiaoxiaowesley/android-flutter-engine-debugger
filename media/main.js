2ebcbdb1bf6169db46b52f8f90d7ddf2cb016ab9
xiaoxiang <xx266218@alibaba-inc.com>
2023-08-18 10:22:27 +0800

    :rotating_light: Format

--- media/main.js
+++ media/main.js
@@ -10,34 +10,34 @@
     const RET_CODE_INPROGRESS = 0;
     const RET_CODE_SUCCESS = 1;
     const REC_CODE_LACK_OF_PARAMETER = -3;
-    
+
     const oldState = vscode.getState() || { colors: [] };
 
     /** @type {Array<{ value: string }>} */
     let colors = oldState.colors;
 
-    document.getElementById('local-engine-input')?.addEventListener('change', function() {
+    document.getElementById('local-engine-input')?.addEventListener('change', function () {
         const p = document.getElementById('local-engine-p');
-        if(p){
+        if (p) {
             p.innerHTML = "<b>local engine:</b>" + this.value;
             vscode.postMessage({ type: 'updateLocalEngine', value: this.value });
         }
     });
 
-     document.getElementById('local-engine-src-path-input')?.addEventListener('change', function() {
+    document.getElementById('local-engine-src-path-input')?.addEventListener('change', function () {
         const p = document.getElementById('local-engine-src-path-p');
-        if(p){
+        if (p) {
             p.innerHTML = "<b>local engine src path:</b>" + this.value;
             vscode.postMessage({ type: 'updateLocalEngineSrcPath', value: this.value });
         }
     });
 
-    document.getElementById('package-name-input')?.addEventListener('change', function() {
+    document.getElementById('package-name-input')?.addEventListener('change', function () {
         const p = document.getElementById('package-name-p');
-        if(p){
+        if (p) {
             p.innerHTML = "<b>package name:</b>" + this.value;
             vscode.postMessage({ type: 'updatePackageName', value: this.value });
-        }        
+        }
     });
 
     document.getElementById('generate-button')?.addEventListener('click', () => {
@@ -52,25 +52,25 @@
                 {
                     const { code, step, log } = message.value;
                     const p = document.getElementById('status');
-                    if(p){
+                    if (p) {
                         p.innerHTML = `${step} ${log}`;
                         p.style.backgroundColor = "black";
-                        if(code === RET_CODE_SUCCESS){
+                        if (code === RET_CODE_SUCCESS) {
                             p.style.color = "green";
-                        }else if(code === RET_CODE_INPROGRESS){
+                        } else if (code === RET_CODE_INPROGRESS) {
                             p.style.color = "white";
-                        } else if(code === RET_CODE_ANDROID_HOME_NOT_FOUND){
+                        } else if (code === RET_CODE_ANDROID_HOME_NOT_FOUND) {
                             p.style.color = "red";
-                        } else if(code === REC_CODE_PACKAGE_NOT_FOUND){
+                        } else if (code === REC_CODE_PACKAGE_NOT_FOUND) {
                             p.style.color = "red";
-                        } else if(code === REC_CODE_LACK_OF_PARAMETER){
+                        } else if (code === REC_CODE_LACK_OF_PARAMETER) {
                             p.style.color = "orange";
                         }
                     }
 
                     break;
                 }
-            
+
         }
     });
 
